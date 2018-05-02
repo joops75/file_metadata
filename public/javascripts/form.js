789619b3c3d73e5a90703bb6f53ee66ab34280cc
Simon Cooper <kidney9@hotmail.com>
2018-05-02 12:59:50 +0100

    submit button hide/fade-in functionality added

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -2,3 +2,9 @@ $(window).bind("pageshow", function() {
     var form = $('form')
     form[0].reset()
 })
+
+$('#upload').hide()
+
+$('#browse').on('change', function() {
+    $('#upload').fadeIn(1000)
+})
